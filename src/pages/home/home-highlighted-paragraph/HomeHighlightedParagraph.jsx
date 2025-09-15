27cfb90f0c09dc2f51d6b2327df459cfbc9d765c
Gerardo Amado <gerardo.amado@icloud.com>
2025-09-14 21:37:07 -0300

    Texto Destacado de ASIMOV

--- src/pages/home/home-highlighted-paragraph/HomeHighlightedParagraph.jsx
+++ src/pages/home/home-highlighted-paragraph/HomeHighlightedParagraph.jsx
@@ -1,9 +1,17 @@
 import React from "react";
 import "./home-highlighted-paragraph.scss";
+import { Text } from "@/components/texts";
 
 const HomeHighlightedParagraph = () => {
     return (
-        <div className="home-highlighted-paragraph">Siempre aportando valor a entornos de innovación e ingeniería. Armón Dirac se especializa en equipos de robótica de alto rendimiento, ayudándolos a alcanzar sus objetivos y creando el entorno adecuado para el máximo desarrollo de sus capacidades. Aportando valor al dinamismo de personas creativas y organizaciones de impacto. </div>
+        <div className="home-highlighted-paragraph">
+            <p className="home-highlighted-paragraph__list">
+                <Text variant="h3">Un robot no hará daño a un ser humano, ni por inacción permitirá que un ser humano sufra daño.</Text>
+                <Text variant="h3">Un robot debe cumplir las órdenes dadas por los seres humanos, a excepción de aquellas que entren en conflicto con la primera ley.</Text>
+                <Text variant="h3">Un robot debe proteger su propia existencia en la medida en que esta protección no entre en conflicto con la primera o con la segunda ley.</Text>
+                <Text variant="h3">ISAAC ASIMOV</Text>
+            </p>
+        </div>
     );
 };
 
